34f7d61ff37c79f2173ae29a4e7b75e2e86e239d
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2012-05-03 14:12:45 +0900

    ap_mrb_request.h created.

--- ap_mrb_string.h
+++ ap_mrb_string.h
@@ -1,23 +1,6 @@
 #include "mruby/string.h"
 
-request_rec *mrb_request_rec_state = NULL;
-
-static int ap_mrb_push_request(request_rec *r);
-request_rec *ap_mrb_get_request();
 mrb_value ap_mrb_rputs(mrb_state *mrb, mrb_value str);
-
-
-static int ap_mrb_push_request(request_rec *r)
-{
-    mrb_request_rec_state = (request_rec *)apr_pcalloc(r->pool, sizeof (*mrb_request_rec_state));
-    mrb_request_rec_state = r;
-    return OK;
-}
-
-request_rec *ap_mrb_get_request()
-{
-    return mrb_request_rec_state;
-}
 
 mrb_value ap_mrb_rputs(mrb_state *mrb, mrb_value str)
 {
