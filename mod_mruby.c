3cc15c9a4fa1e0217478c48fb60db6b48e2a33fa
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2012-06-13 14:31:35 +0900

    ap_mruby_class_init warnings fixed.

--- mod_mruby.c
+++ mod_mruby.c
@@ -67,6 +67,7 @@ apr_global_mutex_t *mod_mruby_mutex;
 cache_table_t *mod_mruby_cache_table = NULL;
 #endif
 
+int ap_mruby_class_init(mrb_state *mrb);
 
 static int mod_mruby_init(apr_pool_t *p, apr_pool_t *plog, apr_pool_t *ptemp, server_rec *s);
 static int ap_mruby_run(mrb_state *mrb, request_rec *r, mruby_config_t *conf, const char *mruby_code_file, int module_status);
