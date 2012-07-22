f616c32ce2572b975e934553c4cf26bbd47678c1
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2012-07-22 13:52:21 +0900

    Create example/worker_rate_limit.rb

--- example/worker_rate_limit.rb
+++ example/worker_rate_limit.rb
@@ -1,24 +1,25 @@
+# LoadModule mruby_module modules/mod_mruby.so
+# ExtendedStatus On
+# mrubyAccessCheckerMiddle /var/www/html/worker_rate_limit.rb
+
 class Worker
     def initialize(rate)
         @rate = rate
     end
     def check
         sb = Apache::Scoreboard.new()
-        busyrate = sb.busy_worker / (sb.process_limit * sb.worker_limit)
-        
-        if busyrate * 100 > @rate
+        busyrate = sb.busy_worker / (sb.server_limit * sb.thread_limit) * 100
+        if busyrate > @rate
             true
         else
             false
         end
     end
 end
- 
-r = Apache::Request.new()
-s = Apache::Server.new()
-w = Worker.new(50)
+
+# busy worker thread rate is 90
+w = Worker.new(90)
 if w.check
-    r.filename = s.document_root + "error/503.html"
     Apache::return(Apache::HTTP_SERVICE_UNAVAILABLE)
 else
     Apache::return(Apache::OK)
