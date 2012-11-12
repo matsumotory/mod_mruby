a6d71520d5378d8bef4ec4d4b71df17f5054df01
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2012-11-12 19:41:18 +0900

    Fix example

--- example/worker_rate_limit.rb
+++ example/worker_rate_limit.rb
@@ -3,36 +3,24 @@
 # mrubyAccessCheckerMiddle /var/www/html/worker_rate_limit.rb
 
 class Worker
-    def initialize(rate, count)
+    def initialize
         @sb = Apache::Scoreboard.new()
-        @myrate = rate
-        @mycount = count
     end
-    def busycheck
-        busyrate = @sb.busy_worker / (@sb.server_limit * @sb.thread_limit) * 100
-        Apache.errlogger(4, "busy worker rate: " + busyrate.to_s)
-        if busyrate > @myrate
-            true
-        else
-            false
-        end
+    def busyrate
+        @sb.busy_worker / (@sb.server_limit * @sb.thread_limit) * 100
     end
-    def accesscheck
-        accesspsec = @sb.total_access / @sb.uptime
-        Apache.errlogger(4, "access per sec: " + accesspsec.to_s)
-        if accesspsec > @mycount
-            true
-        else
-            false
-        end
+    def accessrate
+        @sb.total_access / @sb.uptime
     end
 end
 
 # busy worker thread rate is 90 and access per sec is 1000
-w = Worker.new(90, 1000)
-if w.busycheck
+w = Worker.new()
+if w.busyrate > 90
+    Apache.errlogger(4, "busy worker rate: " + busyrate.to_s)
     Apache::return(Apache::HTTP_SERVICE_UNAVAILABLE)
-elsif w.accesscheck
+elsif w.accessrate > 10000
+    Apache.errlogger(4, "access per sec: " + accesspsec.to_s)
     Apache::return(Apache::HTTP_SERVICE_UNAVAILABLE)
 else
     Apache::return(Apache::DECLINED)
