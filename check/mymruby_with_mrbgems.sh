c123d5f5735aa7c0da7905f183a7631cfea2225b
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2012-12-24 13:13:26 +0900

    Add check dir

--- check/mymruby_with_mrbgems.sh
+++ check/mymruby_with_mrbgems.sh
@@ -6,6 +6,7 @@ cd mruby/mrbgems/g/
 git clone git://github.com/mattn/mruby-uv.git
 git clone git://github.com/mattn/mruby-http.git
 git clone git://github.com/mattn/mruby-json.git
+git clone git://github.com/mattn/mruby-curl.git
 git clone git://github.com/matsumoto-r/mruby-redis.git
 git clone git://github.com/matsumoto-r/mruby-simplehttp.git
 git clone git://github.com/matsumoto-r/mruby-httprequest.git
@@ -19,6 +20,7 @@ mruby-json
 mruby-uv
 mruby-http
 mruby-redis
+mruby-curl
 mruby-simplehttp
 mruby-httprequest
 mruby-growthforecast
