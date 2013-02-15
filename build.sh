ea8aba8d91e4090da5e2efbfe0480ea5bb75e69f
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-02-15 18:46:34 +0900

    Add build.sh

--- build.sh
+++ build.sh
@@ -2,13 +2,28 @@
 
 set -e
 
+if [ -d "./mruby/src" ]; then
+    echo "mruby Downloading ..."
+    git submodule init
+    git submodule update
+    echo "mruby Downloading ... Done"
+fi
 cd mruby
 if [ -d "./build" ]; then
+    echo "mruby Cleaning ..."
     rake clean
+    echo "mruby Cleaning ... Done"
 fi
+echo "mruby building ..."
+mv build_config.rb{,.orig}
+cp ../build_config.rb .
 rake CFLAGS="-fPIC"
+echo "mruby building ... Done"
 cd ..
+echo "mod_mruby building ..."
 ./configure
 make
 make install
-make restart
+echo "mod_mruby building ... Done"
+
+#make restart
