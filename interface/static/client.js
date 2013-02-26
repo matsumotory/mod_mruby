b8316188776d6a2420337d3aeeb1efd074df6e3f
YOSHIHARU Tsuzaki <youchike1220@gmail.com>
2013-02-26 15:39:44 +0900

    add new graph

--- interface/static/client.js
+++ interface/static/client.js
@@ -12,6 +12,8 @@ $(document).ready(function() {
     var gdata = 0;
     var total_access_prev = -1;
     var total_access_curr = -1;
+    var total_kbyte_prev = -1;
+    var total_kbyte_curr = -1;
 
     //socket.on('change', function(val) {
     //    console.log('change:' + val);
@@ -90,6 +92,85 @@ $(document).ready(function() {
                             data.push({
                                 x: time + i * 1000,
                                 y: (total_access_curr - total_access_prev) / 5
+                            });
+                        }
+                        return data;
+                    })()
+                }]
+            });
+        });
+    });
+
+    $(function () {
+        $(document).ready(function() {
+            Highcharts.setOptions({
+                global: {
+                    useUTC: false
+                }
+            });
+
+            var chart;
+            chart = new Highcharts.Chart({
+                chart: {
+                    renderTo: 'container6',
+                    type: 'spline',
+                    marginRight: 10,
+                    events: {
+                        load: function() {
+
+                            // set up the updating of the chart each second
+                            var series = this.series[0];
+                            setInterval(function() {
+                                var x = (new Date()).getTime(), // current time
+                                    //y = Math.random();
+                                    y = (total_kbyte_curr - total_kbyte_prev) / 5;
+                                series.addPoint([x, y], true, true);
+                            }, 1000);
+                        }
+                    }
+                },
+                title: {
+                    text: 'Kbyte per second'
+                },
+                xAxis: {
+                    type: 'datetime',
+                    tickPixelInterval: 150
+                },
+                yAxis: {
+                    title: {
+                        text: 'kbyte/s'
+                    },
+                    plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                },
+                tooltip: {
+                    formatter: function() {
+                            return '<b>'+ this.series.name +'</b><br/>'+
+                            Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
+                            Highcharts.numberFormat(this.y, 2);
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                exporting: {
+                    enabled: false
+                },
+                series: [{
+                    name: 'Random data',
+                    data: (function() {
+                        // generate an array of random data
+                        var data = [],
+                            time = (new Date()).getTime(),
+                            i;
+
+                        for (i = -19; i <= 0; i++) {
+                            data.push({
+                                x: time + i * 1000,
+                                y: (total_kbyte_curr - total_kbyte_prev) / 5
                             });
                         }
                         return data;
@@ -195,6 +276,15 @@ $(document).ready(function() {
 	
 	total_access_curr = val.total_access;
         
+    if(total_kbyte_prev == -1){
+        total_kbyte_prev = val.total_kbyte;
+    }
+    else {
+        total_kbyte_prev = total_kbyte_curr;
+    }
+
+    total_kbyte_curr = val.total_kbyte;
+
 	var chart;
   $(document).ready(function() {
       chart = new Highcharts.Chart({
