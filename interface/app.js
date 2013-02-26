ef5120a555a004d31a83cb0ecf6ae4369be0eacf
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-02-25 22:58:52 -0800

    Refactored code.

--- interface/app.js
+++ interface/app.js
@@ -7,57 +7,57 @@ var http     = require('http');
 var server   = '127.0.0.1';
 var port     = 3000;
 var log_file = '/tmp/tmp.log';
+var interval = 5000;
 
 var app = express();
 var io  = socketio.listen(app.listen(port));
 
+var options = {
+    host: '127.0.0.1',
+    port: 80,
+    path: '/api/get/scoreboard/all'
+};
+
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/static'));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/static'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
 
 app.get('/', function(req, res){
-  res.render('index', {
-    title:  'api_get',
-    server: server,
-    port:   port
-  });
+    res.render('index', {
+        title:  'Apache HTTP Server Status with mod_mruby',
+        server: server,
+        port:   port
+    });
 });
 
-var options = {
-  host: '127.0.0.1',
-  port: 80,
-  path: '/api/get/scoreboard/all'
-};
-
-function get_api() {
-  http.get(options, function(res) {
-    console.log("Got response: " + res.statusCode);
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-      console.log('BODY: ' + chunk);
-      var json_hash = JSON.parse(chunk); 
-      io.sockets.emit('change', json_hash.result);
-      //io.sockets.emit('change', json_hash.result.total_kbyte);
+function call_api() {
+    http.get(options, function(res) {
+        console.log("Got response: " + res.statusCode);
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+            var json_hash = JSON.parse(chunk); 
+            io.sockets.emit('change', json_hash.result);
+        });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
     });
-  }).on('error', function(e) {
-    console.log("Got error: " + e.message);
-  });
-  setTimeout(get_api, 5000);
+    setTimeout(call_api, interval);
 }
 
-get_api();
+call_api();
 
 console.log("start app.js http://%s:%s", server, port);
--- interface/static/client.js
+++ interface/static/client.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
     var socket = io.connect('http://'+ server +':'+ port);
+    var gdata = 0;
+    var total_access_prev = -1;
+    var total_access_curr = -1;
+    var total_kbyte_prev = -1;
+    var total_kbyte_curr = -1;
 
     socket.on('connect', function() {
         console.log('connect');
@@ -8,19 +13,6 @@ $(document).ready(function() {
     socket.on('disconnect', function(){
         console.log('disconnect');
     });
-
-    var gdata = 0;
-    var total_access_prev = -1;
-    var total_access_curr = -1;
-    var total_kbyte_prev = -1;
-    var total_kbyte_curr = -1;
-
-    //socket.on('change', function(val) {
-    //    console.log('change:' + val);
-    //    $('#val').append(val);
-    //    $('#container').append(val);
-    //});
-
 
     $(function () {
         $(document).ready(function() {
@@ -38,12 +30,9 @@ $(document).ready(function() {
                     marginRight: 10,
                     events: {
                         load: function() {
-        
-                            // set up the updating of the chart each second
                             var series = this.series[0];
                             setInterval(function() {
-                                var x = (new Date()).getTime(), // current time
-                                    //y = Math.random();
+                                var x = (new Date()).getTime(),
                                     y = (total_access_curr - total_access_prev) / 5;
                                 series.addPoint([x, y], true, true);
                             }, 1000);
@@ -59,7 +48,7 @@ $(document).ready(function() {
                 },
                 yAxis: {
                     title: {
-                        text: 'Access/s'
+                        text: 'Access/sec'
                     },
                     plotLines: [{
                         value: 0,
@@ -81,9 +70,8 @@ $(document).ready(function() {
                     enabled: false
                 },
                 series: [{
-                    name: 'Random data',
+                    name: 'Access per sec',
                     data: (function() {
-                        // generate an array of random data
                         var data = [],
                             time = (new Date()).getTime(),
                             i;
@@ -117,12 +105,9 @@ $(document).ready(function() {
                     marginRight: 10,
                     events: {
                         load: function() {
-
-                            // set up the updating of the chart each second
                             var series = this.series[0];
                             setInterval(function() {
-                                var x = (new Date()).getTime(), // current time
-                                    //y = Math.random();
+                                var x = (new Date()).getTime(), 
                                     y = (total_kbyte_curr - total_kbyte_prev) / 5;
                                 series.addPoint([x, y], true, true);
                             }, 1000);
@@ -138,7 +123,7 @@ $(document).ready(function() {
                 },
                 yAxis: {
                     title: {
-                        text: 'kbyte/s'
+                        text: 'kbyte/sec'
                     },
                     plotLines: [{
                         value: 0,
@@ -160,9 +145,8 @@ $(document).ready(function() {
                     enabled: false
                 },
                 series: [{
-                    name: 'Random data',
+                    name: 'kbyte per sec',
                     data: (function() {
-                        // generate an array of random data
                         var data = [],
                             time = (new Date()).getTime(),
                             i;
@@ -180,283 +164,199 @@ $(document).ready(function() {
         });
     });
 
-
-    $(function () {
-        $(document).ready(function() {
-            Highcharts.setOptions({
-                global: {
-                    useUTC: false
-                }
-            });
-        
-            var chart;
-            chart = new Highcharts.Chart({
-                chart: {
-                    renderTo: 'container4',
-                    type: 'spline',
-                    marginRight: 10,
-                    events: {
-                        load: function() {
-        
-                            // set up the updating of the chart each second
-                            var series = this.series[0];
-                            setInterval(function() {
-                                var x = (new Date()).getTime(), // current time
-                                    //y = Math.random();
-                                    y = gdata;
-                                series.addPoint([x, y], true, true);
-                            }, 1000);
-                        }
-                    }
-                },
-                title: {
-                    text: 'Live random data'
-                },
-                xAxis: {
-                    type: 'datetime',
-                    tickPixelInterval: 150
-                },
-                yAxis: {
-                    title: {
-                        text: 'Value'
-                    },
-                    plotLines: [{
-                        value: 0,
-                        width: 1,
-                        color: '#808080'
-                    }]
-                },
-                tooltip: {
-                    formatter: function() {
-                            return '<b>'+ this.series.name +'</b><br/>'+
-                            Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
-                            Highcharts.numberFormat(this.y, 2);
-                    }
-                },
-                legend: {
-                    enabled: false
-                },
-                exporting: {
-                    enabled: false
-                },
-                series: [{
-                    name: 'Random data',
-                    data: (function() {
-                        // generate an array of random data
-                        var data = [],
-                            time = (new Date()).getTime(),
-                            i;
-        
-                        for (i = -19; i <= 0; i++) {
-                            data.push({
-                                x: time + i * 1000,
-                                //y: Math.random()
-                                y: gdata
-                            });
-                        }
-                        return data;
-                    })()
-                }]
-            });
-        });
-        
-    });
-
     socket.on('change', function(val) {
         console.log('change:' + val);
         $('#val').append(val);
         gdata = val.idle_worker;
 	
-	if(total_access_prev == -1){
+	if (total_access_prev == -1) {
 		total_access_prev = val.total_access;
-	}
-	else {
+	} else {
 		total_access_prev = total_access_curr;
 	}
 	
 	total_access_curr = val.total_access;
         
-    if(total_kbyte_prev == -1){
+    if (total_kbyte_prev == -1) {
         total_kbyte_prev = val.total_kbyte;
-    }
-    else {
+    } else {
         total_kbyte_prev = total_kbyte_curr;
     }
 
     total_kbyte_curr = val.total_kbyte;
 
 	var chart;
-  $(document).ready(function() {
-      chart = new Highcharts.Chart({
-          chart: {
-              renderTo: 'container7',
-              plotBackgroundColor: null,
-              plotBorderWidth: null,
-              plotShadow: false
-          },
-          title: {
-              text: 'thread status for request'
-          },
-          tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage}%</b>',
-            percentageDecimals: 1
-          },
-          plotOptions: {
-              pie: {
-                  allowPointSelect: true,
-                  cursor: 'pointer',
-                  dataLabels: {
-                      enabled: true,
-                      color: '#000000',
-                      connectorColor: '#000000',
-                      formatter: function() {
-                          return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
-                      }
-                  }
-              }
-          },
-          series: [{
-              type: 'pie',
-              name: 'thread status',
-              data: [
-                  ['SERVER_GRACEFUL',       val.counter.SERVER_GRACEFUL],
-                  ['SERVER_BUSY_READ',      val.counter.SERVER_BUSY_READ],
-                  ['SERVER_IDLE_KILL',      val.counter.SERVER_IDLE_KILL],
-                  //['SERVER_DEAD',           val.counter.SERVER_DEAD],
-                  ['SERVER_BUSY_LOG',       val.counter.SERVER_BUSY_LOG],
-                  ['SERVER_BUSY_DNS',       val.counter.SERVER_BUSY_DNS],
-                  ['SERVER_CLOSING',        val.counter.SERVER_CLOSING],
-                  ['SERVER_STARTING',       val.counter.SERVER_STARTING],
-                  ['SERVER_BUSY_WRITE',     val.counter.SERVER_BUSY_WRITE],
-                  ['SERVER_BUSY_KEEPALIVE', val.counter.SERVER_BUSY_KEEPALIVE],
-                  ['SERVER_READY',          val.counter.SERVER_READY]
-              ]
-          }]
-      });
-  });
-
-        $(document).ready(function() {
-            chart = new Highcharts.Chart({
-                chart: {
-                    renderTo: 'container',
-                    type: 'bar',
-                    animation: false
-                },
-                title: {
-                    text: 'Apache Status Chart'
-                },
-                xAxis: {
-                    categories: ['worker']
-                },
-                yAxis: {
-                    min: 0,
-                    title: {
-                        text: 'Total Apache Status consumption'
+    $(document).ready(function() {
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'container7',
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false
+            },
+            title: {
+                text: 'Thread status for request'
+            },
+            tooltip: {
+              pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+              percentageDecimals: 1
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        color: '#000000',
+                        connectorColor: '#000000',
+                        formatter: function() {
+                            return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
+                        }
                     }
-                },
-                legend: {
-                    backgroundColor: '#FFFFFF',
-                    reversed: true
-                },
-                tooltip: {
-                    formatter: function() {
-                        return ''+
-                            this.series.name +': '+ this.y +'';
-                    }
-                },
-                plotOptions: {
-                    series: {
-                        stacking: 'normal'
-                    }
-                }, 
-                    series: [{
-                    name: 'idle',
-                    data: [val.idle_worker]
-                }, {
-                    name: 'busy',
-                    data: [val.busy_worker]
-                }]
-            });
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'thread status',
+                data: [
+                    ['SERVER_GRACEFUL',       val.counter.SERVER_GRACEFUL],
+                    ['SERVER_BUSY_READ',      val.counter.SERVER_BUSY_READ],
+                    ['SERVER_IDLE_KILL',      val.counter.SERVER_IDLE_KILL],
+                    //['SERVER_DEAD',           val.counter.SERVER_DEAD],
+                    ['SERVER_BUSY_LOG',       val.counter.SERVER_BUSY_LOG],
+                    ['SERVER_BUSY_DNS',       val.counter.SERVER_BUSY_DNS],
+                    ['SERVER_CLOSING',        val.counter.SERVER_CLOSING],
+                    ['SERVER_STARTING',       val.counter.SERVER_STARTING],
+                    ['SERVER_BUSY_WRITE',     val.counter.SERVER_BUSY_WRITE],
+                    ['SERVER_BUSY_KEEPALIVE', val.counter.SERVER_BUSY_KEEPALIVE],
+                    ['SERVER_READY',          val.counter.SERVER_READY]
+                ]
+            }]
         });
+    });
 
-        $(document).ready(function() {
-            chart = new Highcharts.Chart({
-                chart: {
-                    renderTo: 'container2',
-                    type: 'bar'
-                },
+    $(document).ready(function() {
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'container',
+                type: 'bar',
+                animation: false
+            },
+            title: {
+                text: 'Apache busy/idle Worker Chart'
+            },
+            xAxis: {
+                categories: ['worker']
+            },
+            yAxis: {
+                min: 0,
                 title: {
-                    text: 'Apache Status Chart'
-                },
-                xAxis: {
-                    categories: ['total_access']
-                },
-                yAxis: {
-                    min: 0,
-                    title: {
-                        text: 'Total Apache Status consumption'
-                    }
-                },
-                legend: {
-                    backgroundColor: '#FFFFFF',
-                    reversed: true
-                },
-                tooltip: {
-                    formatter: function() {
-                        return ''+
-                            this.series.name +': '+ this.y +'';
-                    }
-                },
-                plotOptions: {
-                    series: {
-                        stacking: 'normal'
-                    }
-                }, 
-                    series: [{
-                    name: 'counter',
-                    data: [val.total_access]
-                }]
-            });
+                    text: 'the number of worker'
+                }
+            },
+            legend: {
+                backgroundColor: '#FFFFFF',
+                reversed: true
+            },
+            tooltip: {
+                formatter: function() {
+                    return ''+
+                        this.series.name +': '+ this.y +'';
+                }
+            },
+            plotOptions: {
+                series: {
+                    stacking: 'normal'
+                }
+            }, 
+                series: [{
+                name: 'idle',
+                data: [val.idle_worker]
+            }, {
+                name: 'busy',
+                data: [val.busy_worker]
+            }]
         });
+    });
 
-        $(document).ready(function() {
-            chart = new Highcharts.Chart({
-                chart: {
-                    renderTo: 'container3',
-                    type: 'bar'
-                },
+    $(document).ready(function() {
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'container2',
+                type: 'bar'
+            },
+            title: {
+                text: 'Apache Total Access'
+            },
+            xAxis: {
+                categories: ['total_access']
+            },
+            yAxis: {
+                min: 0,
                 title: {
-                    text: 'Apache Status Chart'
-                },
-                xAxis: {
-                    categories: ['total_kbyte']
-                },
-                yAxis: {
-                    min: 0,
-                    title: {
-                        text: 'Total Apache Status consumption'
-                    }
-                },
-                legend: {
-                    backgroundColor: '#FFFFFF',
-                    reversed: true
-                },
-                tooltip: {
-                    formatter: function() {
-                        return ''+
-                            this.series.name +': '+ this.y +'';
-                    }
-                },
-                plotOptions: {
-                    series: {
-                        stacking: 'normal'
-                    }
-                }, 
-                    series: [{
-                    name: 'total_kbyte',
-                    data: [val.total_kbyte]
-                }]
-            });
+                    text: 'the number of total access'
+                }
+            },
+            legend: {
+                backgroundColor: '#FFFFFF',
+                reversed: true
+            },
+            tooltip: {
+                formatter: function() {
+                    return ''+
+                        this.series.name +': '+ this.y +'';
+                }
+            },
+            plotOptions: {
+                series: {
+                    stacking: 'normal'
+                }
+            }, 
+                series: [{
+                name: 'the number of access',
+                data: [val.total_access]
+            }]
         });
+    });
+
+    $(document).ready(function() {
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'container3',
+                type: 'bar'
+            },
+            title: {
+                text: 'Apache Total Kbyte'
+            },
+            xAxis: {
+                categories: ['total_kbyte']
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'total kbyte'
+                }
+            },
+            legend: {
+                backgroundColor: '#FFFFFF',
+                reversed: true
+            },
+            tooltip: {
+                formatter: function() {
+                    return ''+
+                        this.series.name +': '+ this.y +'';
+                }
+            },
+            plotOptions: {
+                series: {
+                    stacking: 'normal'
+                }
+            }, 
+                series: [{
+                name: 'total_kbyte',
+                data: [val.total_kbyte]
+            }]
+        });
+    });
     });
 });
