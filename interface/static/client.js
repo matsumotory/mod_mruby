42c637ec8a85771415976fe09e89aa20b0bc5936
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-02-25 17:50:10 -0800

    Add gragh

--- interface/static/client.js
+++ interface/static/client.js
@@ -9,9 +9,55 @@ $(document).ready(function() {
         console.log('disconnect');
     });
 
-    socket.on('change', function(log) {
-        console.log('change:' + log);
-        $('#log').append(log);
+    //socket.on('change', function(val) {
+    //    console.log('change:' + val);
+    //    $('#val').append(val);
+    //    $('#container').append(val);
+    //});
+
+    socket.on('change', function(val) {
+        console.log('change:' + val);
+        $('#val').append(val);
+        var chart;
+        $(document).ready(function() {
+            chart = new Highcharts.Chart({
+                chart: {
+                    renderTo: 'container',
+                    type: 'bar'
+                },
+                title: {
+                    text: 'Stacked bar chart'
+                },
+                xAxis: {
+                    categories: ['Apples']
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Total fruit consumption'
+                    }
+                },
+                legend: {
+                    backgroundColor: '#FFFFFF',
+                    reversed: true
+                },
+                tooltip: {
+                    formatter: function() {
+                        return ''+
+                            this.series.name +': '+ this.y +'';
+                    }
+                },
+                plotOptions: {
+                    series: {
+                        stacking: 'normal'
+                    }
+                }, 
+                    series: [{
+                    name: 'John',
+                    data: [val]
+                }]
+            });
+        });
     });
 
 });
