05418602a60b95a96b590364c6628c39b65abc6c
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-02-25 19:58:28 -0800

    Add new gragh

--- interface/static/client.js
+++ interface/static/client.js
@@ -9,21 +9,105 @@ $(document).ready(function() {
         console.log('disconnect');
     });
 
+    var gdata = 0;
+
     //socket.on('change', function(val) {
     //    console.log('change:' + val);
     //    $('#val').append(val);
     //    $('#container').append(val);
     //});
+    $(function () {
+        $(document).ready(function() {
+            Highcharts.setOptions({
+                global: {
+                    useUTC: false
+                }
+            });
+        
+            var chart;
+            chart = new Highcharts.Chart({
+                chart: {
+                    renderTo: 'container4',
+                    type: 'spline',
+                    marginRight: 10,
+                    events: {
+                        load: function() {
+        
+                            // set up the updating of the chart each second
+                            var series = this.series[0];
+                            setInterval(function() {
+                                var x = (new Date()).getTime(), // current time
+                                    //y = Math.random();
+                                    y = gdata;
+                                series.addPoint([x, y], true, true);
+                            }, 1000);
+                        }
+                    }
+                },
+                title: {
+                    text: 'Live random data'
+                },
+                xAxis: {
+                    type: 'datetime',
+                    tickPixelInterval: 150
+                },
+                yAxis: {
+                    title: {
+                        text: 'Value'
+                    },
+                    plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                },
+                tooltip: {
+                    formatter: function() {
+                            return '<b>'+ this.series.name +'</b><br/>'+
+                            Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
+                            Highcharts.numberFormat(this.y, 2);
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                exporting: {
+                    enabled: false
+                },
+                series: [{
+                    name: 'Random data',
+                    data: (function() {
+                        // generate an array of random data
+                        var data = [],
+                            time = (new Date()).getTime(),
+                            i;
+        
+                        for (i = -19; i <= 0; i++) {
+                            data.push({
+                                x: time + i * 1000,
+                                //y: Math.random()
+                                y: gdata
+                            });
+                        }
+                        return data;
+                    })()
+                }]
+            });
+        });
+        
+    });
 
     socket.on('change', function(val) {
         console.log('change:' + val);
         $('#val').append(val);
+        gdata = val.idle_worker;
         var chart;
         $(document).ready(function() {
             chart = new Highcharts.Chart({
                 chart: {
                     renderTo: 'container',
-                    type: 'bar'
+                    type: 'bar',
+                    animation: false
                 },
                 title: {
                     text: 'Apache Status Chart'
