7ff43d0673a1de4aea87fc87f4ac3624b704fb84
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-05-22 16:04:34 +0900

    Add mruby-config

--- build_config.rb
+++ build_config.rb
@@ -24,6 +24,7 @@ MRuby::Build.new do |conf|
   conf.gem :git => 'git://github.com/mattn/mruby-curl.git'
   conf.gem :git => 'git://github.com/matsumoto-r/mruby-redis.git'
   conf.gem :git => 'git://github.com/matsumoto-r/mruby-sleep.git'
+  conf.gem :git => 'git://github.com/matsumoto-r/mruby-config.git'
 
   # use markdown on mod_mruby
   #conf.gem :git => 'git://github.com/matsumoto-r/mruby-discount.git'
