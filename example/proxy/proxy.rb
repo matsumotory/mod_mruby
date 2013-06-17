e0b45e6224aff78bf3d7d32a8bc11f2928833d67
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-06-17 17:00:29 +0900

    Update proxy.rb

--- example/proxy/proxy.rb
+++ example/proxy/proxy.rb
@@ -4,9 +4,7 @@ backends = [
     "http://192.168.0.103:8888/",
     "http://192.168.0.104:8888/",
 ]
- 
-# この辺に色々条件を入れたり、backends配列から取り出すルールを別途定義したりするとmod_mrubyのうまみがでる？
- 
+
 r = Apache::Request.new()
  
 r.handler  = "proxy-server"
