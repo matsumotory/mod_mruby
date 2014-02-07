f814a2ffd2141742f2cf62d8f5cb3071b85eefde
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-02-07 19:21:12 +0900

    fix example using Apache::Redis

--- example/auth/authn_basic_by_redis.rb
+++ example/auth/authn_basic_by_redis.rb
@@ -17,7 +17,7 @@ host    = "127.0.0.1"
 port    = 6379
 
 anp     = Apache::AuthnProvider.new
-redis   = Apache::Redis.new(host, port)
+redis   = Redis.new(host, port)
 
 if redis.get(anp.user) == anp.password
   Apache.return(Apache::AuthnProvider::AUTH_GRANTED)
