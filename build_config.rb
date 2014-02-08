29c460626ec74c13b548df3070026a4dc7940343
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-02-08 20:10:33 +0900

    comment out mruby-io becase of error of mrb_class_get and add mruby-mod-mruby-ext

--- build_config.rb
+++ build_config.rb
@@ -20,12 +20,13 @@ MRuby::Build.new do |conf|
   conf.gem :git => 'git://github.com/iij/mruby-process.git'
   conf.gem :git => 'git://github.com/iij/mruby-pack.git'
   conf.gem :git => 'git://github.com/iij/mruby-digest.git'
-  conf.gem :git => 'git://github.com/iij/mruby-io.git'
+  #conf.gem :git => 'git://github.com/iij/mruby-io.git'
   conf.gem :git => 'git://github.com/mattn/mruby-json.git'
   conf.gem :git => 'git://github.com/matsumoto-r/mruby-redis.git'
   conf.gem :git => 'git://github.com/matsumoto-r/mruby-vedis.git'
   conf.gem :git => 'git://github.com/matsumoto-r/mruby-sleep.git'
   conf.gem :git => 'git://github.com/matsumoto-r/mruby-userdata.git'
+  conf.gem :git => 'git://github.com/matsumoto-r/mruby-mod-mruby-ext.git'
   conf.gem :git => 'git://github.com/masamitsu-murase/mruby-hs-regexp.git'
 
   # use markdown on mod_mruby
