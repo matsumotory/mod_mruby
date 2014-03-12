b4bfda75e03dd7220949edf56eff0b5a5270a3c5
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-03-12 21:47:34 +0900

    Add mruby-memcached

--- build_config.rb
+++ build_config.rb
@@ -25,16 +25,16 @@ MRuby::Build.new do |conf|
   conf.gem :git => 'git://github.com/mattn/mruby-json.git'
   conf.gem :git => 'git://github.com/matsumoto-r/mruby-redis.git'
   conf.gem :git => 'git://github.com/matsumoto-r/mruby-vedis.git'
+  conf.gem :git => 'git://github.com/matsumoto-r/mruby-memcached.git'
   conf.gem :git => 'git://github.com/matsumoto-r/mruby-sleep.git'
   conf.gem :git => 'git://github.com/matsumoto-r/mruby-userdata.git'
   conf.gem :git => 'https://github.com/mattn/mruby-onig-regexp.git'
 
   # mod_mruby extended class
-  conf.gem :git => 'git://github.com/matsumoto-r/mruby-mod-mruby-ext.git'
+  #conf.gem :git => 'git://github.com/matsumoto-r/mruby-mod-mruby-ext.git'
 
   # use markdown on mod_mruby
   #conf.gem :git => 'git://github.com/matsumoto-r/mruby-discount.git'
-  conf.gem '../../mruby-discount'
 
   # Linux only for mod_mruby
   #conf.gem :git => 'git://github.com/matsumoto-r/mruby-capability.git'
