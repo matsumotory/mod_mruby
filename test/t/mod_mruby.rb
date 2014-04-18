32839d72e2c2bd9d46452fefc59961c7048c9771
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-04-19 03:07:27 +0900

    Change port for user permission

--- test/t/mod_mruby.rb
+++ test/t/mod_mruby.rb
@@ -1,7 +1,7 @@
 ##
 # ngx_mruby test
 
-base = 'http://127.0.0.1'
+base = 'http://127.0.0.1:48080'
 
 assert('mod_mruby', 'location /hello-inline') do
   res = HttpRequest.new.get base + '/hello-inline'
