6996337678cc0ca70f6a0baebd59253f8f6ce04b
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-05-01 01:13:25 -0700

    Fix bug; any other test was influenced by document_root test

--- test/htdocs/document_root.rb
+++ test/htdocs/document_root.rb
@@ -1,9 +1,12 @@
 r = Apache::Request.new
 
+orig = r.document_root
 r.document_root = "/tmp"
 
 if r.document_root == "/tmp"
+  r.document_root = orig
   Apache.rputs "OK"
 else
+  r.document_root = orig
   Apache.rputs "NG"
 end
