51555c6d475ef55212d97931fb4cc68856f5228a
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-05-15 21:23:05 +0900

    Bump up 0.9.66

--- src/mod_mruby.h
+++ src/mod_mruby.h
@@ -8,7 +8,7 @@
 #define MOD_MRUBY_H
 
 #define MODULE_NAME    "mod_mruby"
-#define MODULE_VERSION   "0.9.5"
+#define MODULE_VERSION   "0.9.6"
 #define UNSET        -1
 #define SET        1
 #define ON         1
