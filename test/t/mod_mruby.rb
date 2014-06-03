5e42e2d30e17033679c9fbb14107149379bf9e5c
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-06-03 01:26:12 -0700

    Support current mruby-uname

--- test/t/mod_mruby.rb
+++ test/t/mod_mruby.rb
@@ -94,7 +94,7 @@ assert('mod_mruby', 'location /scoreboard-obj') do
   assert_equal "Apache::Scoreboard", res.body
 end
 
-if Uname.new.sysname == 'Linux'
+if Uname.sysname == 'Linux'
   assert('mod_mruby', 'location /tid') do
     res = HttpRequest.new.post base + '/tid'
     assert_true res.body.to_i > 0
