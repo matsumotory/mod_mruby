80809fe258c0a32570474384e659f51b3814f3f2
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-04-08 07:16:21 +0000

    Bump up version to v1.10.8

--- src/mod_mruby.h
+++ src/mod_mruby.h
@@ -8,7 +8,7 @@
 #define MOD_MRUBY_H
 
 #define MODULE_NAME    "mod_mruby"
-#define MODULE_VERSION   "1.10.7"
+#define MODULE_VERSION   "1.10.8"
 #define UNSET        -1
 #define SET        1
 #define ON         1
